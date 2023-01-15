5f1af334002e79ef714e4b84e8aa39ebbef42965
Rtuta Noguchi <ryuta018@gmail.com>
2023-01-15 17:23:23 +0900

    アニメーションの修正

--- resources/views/valentine.blade.php
+++ resources/views/valentine.blade.php
@@ -11,6 +11,38 @@
     <!-- AR.js ライブラリの読み込み -->
     <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
     <script src="https://cdn.rawgit.com/donmccurdy/aframe-extras/v5.0.0/dist/aframe-extras.min.js"></script>
+    <script>
+      AFRAME.registerComponent('registerevents', {
+        init: function () {
+            var marker = this.el;
+
+            // マーカーを検出したイベントの登録
+            marker.addEventListener('markerFound', function () {
+                var markerId = marker.id;
+                console.log('markerFound', markerId);
+
+                // アニメーションの開始
+                document.querySelector('#image').emit('markerfound');
+                document.querySelector('#box1').emit('markerfound');
+                document.querySelector('#box2').emit('markerfound');
+                document.querySelector('#box3').emit('markerfound');
+                document.querySelector('#box4').emit('markerfound');
+                document.querySelector('#box5').emit('markerfound');
+                document.querySelector('#box6').emit('markerfound');
+                document.querySelector('#box7').emit('markerfound');
+                document.querySelector('#box8').emit('markerfound');
+                document.querySelector('#box9').emit('markerfound');
+                document.querySelector('#box10').emit('markerfound');
+                document.querySelector('#box11').emit('markerfound');
+                document.querySelector('#box12').emit('markerfound');
+                document.querySelector('#box13').emit('markerfound');
+                document.querySelector('#box14').emit('markerfound');
+                document.querySelector('#box15').emit('markerfound');
+                document.querySelector('#box16').emit('markerfound');
+            });
+        }
+      });
+    </script>
 
     <!-- A-Frame の VR空間に AR.js を紐づける（デバッグUIは非表示） -->
     <a-scene embedded arjs="debugUIEnabled:false;">
@@ -20,12 +52,14 @@
         <a-asset-item id="ribbon" src={{ asset('/ribbon.obj') }}></a-asset-item>
         <a-asset-item id="mtl" src={{ asset('/ribbon.mtl') }}></a-asset-item>
       </a-assets>
+
       <!-- マーカーを登録（プリセットされている「hiro」マーカー） -->
-      <a-marker type="pattern" url={{ asset('/pattern-ar.patt') }}>
+      <a-marker type="pattern" url={{ asset('/pattern-ar.patt') }} registerevents>
         
-      <a-entity>
+        <a-entity>
           <!-- 手紙 -->
           <a-image
+            id="image"
             src="#levtech"
             rotation="-90 0 0"
           ></a-image>
@@ -34,19 +68,21 @@
             to="30 0 0"
             dur="4000"
             delay="3000"
+            begin="markerfound"
           ></a-animation>
           <a-animation
             attribute="position"
             to="0 2 0"
             dur="4000"
             delay="3000"
+            begin="markerfound"
           ></a-animation>
           <a-animation
             attribute="scale"
-            from="1 1 1"
             to="1.5 1.5 1.5"
             dur="4000"
             delay="3000"
+            begin="markerfound"
           ></a-animation>
         </a-entity>
 
@@ -54,6 +90,7 @@
         <a-entity>
         <a-entity>
           <a-box
+            id="box1"
             position="0 -0.74 0"
             color="#ffffff"
             width="1.5"
@@ -61,6 +98,7 @@
             depth="1.5"
           ></a-box>
           <a-box
+            id="box2"
             position="0 0 0.7"
             color="#ffffff"
             width="1.5"
@@ -68,6 +106,7 @@
             depth="0.1"
           ></a-box>
           <a-box
+            id="box3"
             position="0 0 -0.7"
             color="#ffffff"
             width="1.5"
@@ -75,6 +114,7 @@
             depth="0.1"
           ></a-box>
           <a-box
+            id="box4"
             position="0.7 0 0"
             color="#ffffff"
             width="0.1"
@@ -82,6 +122,7 @@
             depth="1.5"
           ></a-box>
           <a-box
+            id="box5"
             position="-0.7 0 0"
             color="#ffffff"
             width="0.1"
@@ -89,6 +130,7 @@
             depth="1.5"
           ></a-box>
           <a-box
+            id="box6"
             position="0 -0.1 0.78"
             color="#ff0000"
             width="0.3"
@@ -96,6 +138,7 @@
             depth="0.02"
           ></a-box>
           <a-box
+            id="box7"
             position="0 -0.1 -0.78"
             color="#ff0000"
             width="0.3"
@@ -103,6 +146,7 @@
             depth="0.02"
           ></a-box>
           <a-box
+            id="box8"
             position="0.78 -0.1 0"
             color="#ff0000"
             width="0.02"
@@ -110,6 +154,7 @@
             depth="0.3"
           ></a-box>
           <a-box
+            id="box9"
             position="-0.78 -0.1 0"
             color="#ff0000"
             width="0.02"
@@ -122,6 +167,7 @@
         <a-entity>
           <a-obj-model position="0 0.65 -1.3" scale="0.8 1 0.8" rotation="0 180 0" src="#ribbon" mtl="#mtl" color="#ff0000"></a-obj-model>
           <a-box
+            id="box10"
             position="0 0.75 0"
             color="#ffffff"
             width="1.5"
@@ -129,6 +175,7 @@
             depth="1.5"
           ></a-box>
           <a-box
+            id="box11"
             position="0 0.9 0"
             color="#ff0000"
             width="1.59"
@@ -136,6 +183,7 @@
             depth="0.3"
           ></a-box>
           <a-box
+            id="box12"
             position="0 0.9 0"
             color="#ff0000"
             width="0.3"
@@ -143,6 +191,7 @@
             depth="1.59"
           ></a-box>
           <a-box
+            id="box13"
             position="0 0.75 0.78"
             color="#ff0000"
             width="0.3"
@@ -150,6 +199,7 @@
             depth="0.02"
           ></a-box>
           <a-box
+            id="box14"
             position="0 0.75 -0.78"
             color="#ff0000"
             width="0.3"
@@ -157,6 +207,7 @@
             depth="0.02"
           ></a-box>
           <a-box
+            id="box15"
             position="0.78 0.75 0"
             color="#ff0000"
             width="0.02"
@@ -164,6 +215,7 @@
             depth="0.3"
           ></a-box>
           <a-box
+            id="box16"
             position="-0.78 0.75 0"
             color="#ff0000"
             width="0.02"
@@ -176,6 +228,7 @@
             to="0 20 -180"
             dur="4000"
             delay="3000"
+            begin="markerfound"
           ></a-animation>
           <a-animation
             attribute="position"
@@ -183,12 +236,14 @@
             to="1.2 0.3 0"
             dur="700"
             delay="3000"
+            begin="markerfound"
           ></a-animation>
           <a-animation
             attribute="position"
             to="2.3 0.2 0"
             dur="4000"
             delay="3000"
+            begin="markerfound"
           ></a-animation>
         </a-entity>
 
@@ -197,5 +252,6 @@
       <!-- AR用のカメラを置く -->
       <a-entity camera></a-entity>
     </a-scene>
+
   </body>
 </html>
