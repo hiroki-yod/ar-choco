9b7ab4ddf79fbe1578a5d8fc972a2fd38ad931b9
grifis <fujeba2abm@i.softbank.jp>
2023-01-27 01:48:55 +0900

    画像受信テスト

--- app/Http/Controllers/LineWebhookController.php
+++ app/Http/Controllers/LineWebhookController.php
@@ -8,6 +8,9 @@ use App\Services\LineBotService as LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Cloudinary;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Image;
 
 class LineWebhookController extends Controller
 {
@@ -44,23 +47,28 @@ class LineWebhookController extends Controller
                         $arrayContentType = explode('/', $contentType);
                         $ext = end($arrayContentType);
 
+                        $contentType = $response->getHeader('content-type');
+                        $arrayContentType = explode('/', $contentType);
+                        $ext = end($arrayContentType);
+                        $path = 'public/line/' .$event['message']['id'] .'.' .$ext;
+                        Storage::put($path, $response->getRawBody());
+                        Storage::url($path);
 
-                        $image_url = Cloudinary::upload($request->file('image')[0]->getRealPath())->getSecurePath();
-                        $image = Image::create([
-                            "id" => str()->uuid(),
-                            "image_url" => $image_url
-                        ]);
-                        $qr = QrCode::format('png')->size(300)->generate("https://ar-choco.herokuapp.com/valentine/".strval($image->id), '../public/QR/' . strval($image->id) . '.png');
+                        $image_url = Cloudinary::upload(url(Storage::url($path)))->getSecurePath();
+                        // $image = Image::create([
+                        //     "id" => str()->uuid(),
+                        //     "image_url" => $image_url
+                        // ]);
+                        // $qr = QrCode::format('png')->size(300)->generate("https://ar-choco.herokuapp.com/valentine/".strval($image->id), '../public/QR/' . strval($image->id) . '.png');
 
                         $replying_message = new ImageMessageBuilder(
-                            asset("/QR/{$image->id}.svg"),
-                            asset("/QR/{$image->id}.svg")
+                            'https://chart.apis.google.com/chart?chs=500x500&cht=qr&chl=https://a452-125-102-201-146.jp.ngrok.io/storage/line/17535944173884.jpeg',
+                            'https://chart.apis.google.com/chart?chs=240x240&cht=qr&chl=https://a452-125-102-201-146.jp.ngrok.io/storage/line/17535944173884.jpeg'
                         );
                         // $response = $bot->replyText($event['replyToken'], '画像を受け取ったよ');
-                        $response = $bot->replyMessage($event['replyToken'], $replying_message);
+                        $bot->replyMessage($event['replyToken'], $replying_message);
                     } else {
                         error_log($response->getHTTPStatus());
-                        $response = $bot->replyText($event['replyToken'],"{$response->getHTTPStatus()}");
                     }
                     break;
 
