bb42b2254cb90c5d9dc0d02c538e5525dfdc6554
YuugouOhno <yuugou.ohno@gmail.com>
2023-01-29 11:43:25 +0900

    カードの選択をワンクリックで

--- resources/js/Components/SelectTemplate.jsx
+++ resources/js/Components/SelectTemplate.jsx
@@ -7,7 +7,7 @@ const SelectTemplate = ({
     return (
         <>
             <div className="w-full h-full justify-center items-center flex flex-col">
-                <p>メッセージカードのデザインを選ぶ</p>
+                <p class="mt-12 mb-6 text-3xl">メッセージカードのデザインを選ぶ</p>
                 <div className="flex flex-wrap px-4 sm:px-28">
                     {templates.map((template) => {
                         return (
@@ -16,13 +16,7 @@ const SelectTemplate = ({
                                 className="w-1/2 sm:w-1/3 p-3"
                                 onClick={selectTemplate}
                             >
-                                <div
-                                    className={
-                                        template == selectedTemplate
-                                            ? "border-2 border-red-500"
-                                            : ""
-                                    }
-                                >
+                                <div>
                                     <img
                                         src={template}
                                         onClick={changeTemplate}
