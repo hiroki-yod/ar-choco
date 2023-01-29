891dbacb9aa0a0bb4fc8cb4b3808c543d9476913
YuugouOhno <yuugou.ohno@gmail.com>
2023-01-29 10:48:25 +0900

    上に戻るボタンの追加

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import Header from "@/Components/Header";
+import ToTop from "@/components/ToTop";
 
 const Layout = ({ children }) => {
     return (
         <div>
             <Header></Header>
             {children}
+            <ToTop></ToTop>
         </div>
     );
 };
--- resources/js/Pages/Letter/Show.jsx
+++ resources/js/Pages/Letter/Show.jsx
@@ -4,24 +4,33 @@ import Marker from "@/Components/Marker";
 import React from "react";
 import { Head } from "@inertiajs/inertia-react";
 import Layout from "@/Layouts/Layout";
+import ChocoButton from "@/Components/ChocoButton";
 
 const Show = (props) => {
     const { letter } = props;
-    const message = 'チョコが届きました！\n下記urlでカメラを起動してmeijiの板チョコかARマーカーにかざしてみてね!\n' + location.href
+    const message =
+        "チョコが届きました！\n下記urlでカメラを起動してmeijiの板チョコかARマーカーにかざしてみてね!\n" +
+        location.href;
     return (
-        <div className="text-center">
+        <div className="text-center flex flex-col justify-center">
             <Head>
                 <title>きゅーあーるせいせい</title>
             </Head>
-            <div className="my-8 mx-auto w-3/4 mt-20 mb-20">
+            <div className="my-8 mx-auto w-3/4 mt-20 mb-1">
                 <a
                     href={"http://line.me/R/msg/text/?" + message}
                     className="text-4xl py-8 px-8 title-border-red "
                 >
-                    LINEであの人に送る<img className="inline-block ml-3 mb-2" src="/LINE_Brand_icon.png" width="50" height="50"></img>
+                    LINEであの人に送る
+                    <img
+                        className="inline-block ml-3 mb-2"
+                        src="/LINE_Brand_icon.png"
+                        width="50"
+                        height="50"
+                    ></img>
                 </a>
             </div>
-            <p>
+            <p className="mx-auto w-3/4 mt-20 mb-5">
                 LINE以外のツールで送る人は、以下のメッセージと画像を一緒に送ってね
             </p>
             <CoppyBox>{message}</CoppyBox>
@@ -30,19 +39,21 @@ const Show = (props) => {
                     href={`/QR/${letter.id}.svg`}
                     src={`/QR/${letter.id}.svg`}
                 >
-                    QRコードはこちら<DownloadButton href={`/QR/${letter.id}.svg`} />
+                    QRコードはこちら
+                    <DownloadButton href={`/QR/${letter.id}.svg`} />
                 </Marker>
                 <Marker href={`/pattern-ar.png`} src={`/pattern-ar.png`}>
-                    ARマーカーはこちら<DownloadButton href={`/pattern-ar.png`} />
+                    ARマーカーはこちら
+                    <DownloadButton href={`/pattern-ar.png`} />
                 </Marker>
             </div>
+<div className="flex justify-center text-center">
 
-            <a href="/">
-                <p className="text-blue-700 text-lg underline hover:font-bold mb-4">
-                    TOPページに戻る
-                </p>
-            </a>
+            <ChocoButton href="/" className="my-8 mx-auto w-3/4 mt-20 mb-20">
+                TOPページに戻る
+            </ChocoButton>
         </div>
+</div>
     );
 };
 
